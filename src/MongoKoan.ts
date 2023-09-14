24285c9cc0a84a986232e5b6a3662fdb61a0a7db
Mike Storey <mike@ssgpp.com>
2023-09-14 14:43:40 -0400

    initial instructions

--- src/MongoKoan.ts
+++ src/MongoKoan.ts
@@ -16,6 +16,10 @@ export class MongoKoan {
 
   public async connect(): Promise<Array<IndexInformationOptions> | {error: any}> {
     try {
+      // this method should:
+      //  connect this.db
+      //  initilize this.client
+      //  return a list of indexes
       throw("To Be Implemented")
     } catch (error) {
       return {"error": error};
@@ -24,6 +28,9 @@ export class MongoKoan {
 
   public async loadAll(): Promise<InsertManyResult<ProductWithId> | { error: any }> {
     try {
+      // this method should 
+      //  load the productsData into the Koan_products collection 
+      //  return the InsertManyResult array
       throw("To Be Implemented")
     } catch (error) {
       return { error };
@@ -32,6 +39,9 @@ export class MongoKoan {
 
   public async getAll(): Promise< Array<ProductWithId> | {error: any}> {
     try {
+      // this method should
+      //  find all the documents in the Koan_proudcts collection
+      //  return them as an Array
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -40,6 +50,9 @@ export class MongoKoan {
 
   public async countAll(): Promise< number | {error: any}> {
     try {
+      // this method should
+      //  count all the records in the Koan_products collection
+      //  return the count
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -48,6 +61,9 @@ export class MongoKoan {
 
   public async addOne(product: Product): Promise<InsertOneResult<Product> | { error: any }> {
     try {
+      // this method should
+      //  add the prodcut provided to the Koan_products collection
+      //  return the result
       throw("To Be Implemented")      
     } catch (error) {
       return {"error": error };
@@ -56,6 +72,7 @@ export class MongoKoan {
 
   public async getOne(id: string): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should get a Product with the id provided
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -64,6 +81,11 @@ export class MongoKoan {
 
   public async setInStock(id: string, quantity: number): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should
+      //  find the product with the id provided
+      //  add the inStock property
+      //  set the inStock value to the quantity provided
+      //  return the updated product
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -72,6 +94,10 @@ export class MongoKoan {
 
   public async decrementInventoryQuantity(id: string, quantity: number): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should
+      //  find the product with the provided id
+      //  decrement the inventoryQuantity by the amount provided
+      //  return the updated document
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -80,6 +106,10 @@ export class MongoKoan {
 
   public async addTags(id: string, tags: Array<string>): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should
+      //  add a tags attribute to the product with the id provided
+      //  set the value of that tags attribute to the array provided
+      //  return the updated product
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -88,6 +118,9 @@ export class MongoKoan {
 
   public async pushTag(id: string, tag: string): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should
+      //  add a single value to the tags array
+      //  return the updated product
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -96,6 +129,9 @@ export class MongoKoan {
 
   public async pushTags(id: string, tags: Array<string>): Promise<ProductWithId | {error: any}> {
     try {
+      // this method should
+      //  add a list of values to the tags array
+      //  return the updated product
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -104,6 +140,9 @@ export class MongoKoan {
 
   public async deleteOne(id: string): Promise<DeleteResult | {error: any}>{
     try {
+      // this method should
+      //  delete the product with the id provided
+      //  return the DeleteResult 
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -112,6 +151,9 @@ export class MongoKoan {
 
   public async getWithProjection(minimumName: string, fields: Array<string>): Promise<Array<ProductWithId> | {error: any}> {
     try {
+      // this method should
+      //  find all products with a name greater than or equal to the minimumName provided
+      //  create a projection that includes only the fields provided 
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -120,6 +162,11 @@ export class MongoKoan {
 
   public async elemMatch(description: string, minimumCredit: number): Promise<Array<ProductWithId> | {error: any}> {
     try {
+      // this method should
+      //  find all products that have a transaction with
+      //    the description matches the value provided
+      //    a credit value greater than the minimumCredit value provided
+      //  return the product array
       throw("To Be Implemented")
     } catch (error) {
       return {"error":error};
@@ -127,6 +174,8 @@ export class MongoKoan {
   }
 
   public async inMatch(statsOptions: Array<string>): Promise<Array<ProductWithId> | {error: any}> {
+    // this method should
+    //  find all the products with a Status that is in the list of status provided in statusOptions
     try {
       throw("To Be Implemented")
     } catch (error) {
@@ -135,6 +184,10 @@ export class MongoKoan {
   }
 
   public async aggregateSortAdd(addTextValue: string, status: string): Promise<Array<{name: string, status: string, added: string}> | {error: any}> {
+    // this method should use an Aggregation to
+    //  find all products with a status of ?
+    //  add a added attribute to the results, with a value provided in addTextValue
+    //  create a projection that includes only the name, status, and added attributes
     try {
       throw("To Be Implemented")
     } catch (error) {
